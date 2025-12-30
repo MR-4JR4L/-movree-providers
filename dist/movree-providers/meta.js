9d2560c162d54c17efddad7b3dbeafed493ee11f
MR-4JR4L <75429739+MR-4JR4L@users.noreply.github.com>
2025-12-30 18:50:29 +0700

    Create meta.js

---
+++ dist/movree-providers/meta.js
@@ -0,0 +1,7 @@
+async function meta(link) {
+    return {
+        title: "Judul Film",
+        plot: "Sinopsis film...",
+        cast: ["Aktor 1", "Aktor 2"],
+        episodes: [] // Jika serial};
+}
