d5d230c4ddd8a8b76a4bb3bbf86978de97b1536f
MR-4JR4L <75429739+MR-4JR4L@users.noreply.github.com>
2025-12-30 18:49:47 +0700

    Create catalog.js

---
+++ dist/movree-providers/catalog.js
@@ -0,0 +1,12 @@
+async function catalog(filter, page) {
+    // 1. Ambil data dari website film target menggunakan fetch
+    // 2. Olah datanya (Parsing HTML)
+    // 3. Kembalikan array berisi objek film
+    return [
+        {
+            title: "Judul Film",
+            link: "https://website-film.com/film-a",
+            poster: "https://gambar.com/poster.jpg"
+        }
+    ];
+}
